9cf811c04706a0f2eb7f25e000fa8234bde1e502
Jack T <jxtowler@students.everettcc.edu>
2026-02-23 23:43:21 +0000

    Snorlax

---
+++ class.py
@@ -0,0 +1,11 @@
+#Basic for Loop
+#A for loop is used for iterating over a set of range of values
+grocery_list = ["eggs", "milk", "pizza", "cereal"]
+#for each item x, print the item.
+for x in grocery_list:
+    print(x)
+
+
+#Printing each letter in a word
+for x in "Snorlax":
+    print(x)
