9902fdc7655b8617a31afb377c08c360005b409e
Jack T <jxtowler@students.everettcc.edu>
2026-02-24 02:07:42 +0000

    Finished product

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # STEM103_Level3_1-ForLoop
+
+Hello, my name is Jack Towler. Today is 2/23/25, and the current assignment is relatively simple (Supposedly). I've just finished the in class work, and will be working on both this assignment and the previous (Functions) assignment. Hopefully I can do well and get things done. 
--- main.py
+++ main.py
@@ -2,5 +2,16 @@ mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
 mission_success = [True, False, True, True, True, True, False]
 
-#This is doable in 10 lines. 
+#This is doable in 10 or 11 lines - Just use for loops.
 
+total = len(mission_names); success = 0
+for t in range(total):
+    if mission_success[t]: success += 1
+#Variable used is t for total
+print(f"Total number of missions: ", len(mission_names))
+print("\n", f"Number of successful missions: {success}")
+print("\n", f"Success Rate: {success/total*100:.2f}%")
+print("\n", f"Missions before 2000: ")
+#initally had an error, but it was because I forgot to use f before the text. 
+for t in range(total):
+     if mission_years[t] < 2000: print(f"{mission_names[t]}")
